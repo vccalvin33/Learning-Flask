d684f26a60e7fa7063b55fbad1d82396ea1ef0a0
Vincentius Christopher Calvin <vc_calvin@yahoo.com>
2021-06-22 13:14:55 +0700

    add chapter 9

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['vc_calvin@yahoo.com']
+    POSTS_PER_PAGE = 3
