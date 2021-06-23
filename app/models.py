f97f87422142d692d4d187ae118b00e7330ab535
Vincentius Christopher Calvin <vc_calvin@yahoo.com>
2021-06-23 16:20:08 +0700

    add flask-mail, reset password but still unusable due to SMTP connection

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-from app import db, login
+from time import time
+from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+
+import jwt
 
 followers = db.Table('followers', 
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -47,6 +50,20 @@ class User(db.Model,  UserMixin):
         followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter((followers.c.follower_id == self.id))
         own = Post.query.filter_by(user_id = self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
